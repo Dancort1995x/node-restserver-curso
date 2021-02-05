6760830631003d21d95dbe5a33d1e7b5b4020f0b
Daniel <cortesdaniel592@gmail.com>
2021-02-05 12:10:22 -0300

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,11 +3,22 @@ const app = express();
 const Usuario = require('../models/usuario');
 const bycrpt = require('bcrypt');
 const _ = require('underscore');
-const usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion')
 
-app.get('/usuario', function(req, res) {
+
+
+
+
+app.get('/usuario', verificaToken, (req, res) => {
 
     //{estado: true}
+
+    // return res.json({
+    //     usuario: req.usuario,
+    //     nombre: req.usuario.nombre,
+    //     email: req.usuario.email
+    // })
+
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -40,7 +51,7 @@ app.get('/usuario', function(req, res) {
         })
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
@@ -69,7 +80,7 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -94,7 +105,7 @@ app.put('/usuario/:id', function(req, res) {
     // });
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
 
     let id = req.params.id;
